efaac0d89fb9363515047fb6f2358bd1cedb7b0b
Ramadhan Putra <91359996+lanang309@users.noreply.github.com>
2022-06-12 13:27:10 +0700

    Add files via upload

---
+++ application/views/matakuliah/tampil.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mata Kuliah</title>
+
+<body>
+<h3 class="col md-12 text-center">Mata Kuliah</h3>
+<div class="col-md-12">
+    <table style="text-align: center;" border="1" class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>SKS</th>
+                <th>Kode</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $nomor = 1;
+            foreach ($matakuliah as $matkul) {
+            ?>
+            <tr>
+                <td><?php echo $nomor ?></td>
+                <td><?php echo $matkul -> nama ?></td>
+                <td><?php echo $matkul -> sks ?></td>
+                <td><?php echo $matkul -> kode ?></td>
+            </tr>
+            <?php
+            $nomor++;
+            }
+            ?>            
+        </tbody>
+    </table>
+
+    </div>
+</body>
+</html>
